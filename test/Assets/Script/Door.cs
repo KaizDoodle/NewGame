8383459ff245f7edd776469aedfd50875ac1b677
KaizDoodle <13iak13@gmail.com>
2024-02-02 13:16:04 -0800

    WIP commit (work in progress

--- test/Assets/Script/Door.cs
+++ test/Assets/Script/Door.cs
@@ -8,21 +8,22 @@ using UnityEngine.SceneManagement;
 //when something get into the alta, make the runes glow
 
 
-    public class PropsAltar : MonoBehaviour
+    public class Door : MonoBehaviour
     {
         public string LevelName;
-
+        public PlayerPosition playerPosition; // Reference to the PlayerPosition script
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             LoadLevel();
+            playerPosition.PlayerIsSwitchingScene(); // Save player position
         }
         public void LoadLevel()
         {
+            
             SceneManager.LoadScene(LevelName);
+            playerPosition.PlayerIsComingBack(); // Save player position
         }
         
 
     }
-
-    
---
+++ test/Assets/Script/Player poz switch script.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerPosition : MonoBehaviour
+{
+    private Transform player;
+
+    void Start()
+    {
+        player = GetComponent<Transform>();
+        PlayerIsComingBack();
+    }
+
+    public void PlayerIsSwitchingScene()
+    {
+        PlayerPrefs.SetFloat("X", player.position.x);
+        PlayerPrefs.SetFloat("Y", player.position.y);
+        PlayerPrefs.SetFloat("Z", player.position.z);
+        // Player Switches Scene
+    }
+
+    public void PlayerIsComingBack()
+    {
+        // Player comes back
+        float x = PlayerPrefs.GetFloat("X");
+        float y = PlayerPrefs.GetFloat("Y");
+        float z = PlayerPrefs.GetFloat("Z");
+        player.position = new Vector3(x, y, z);
+    }
+}
