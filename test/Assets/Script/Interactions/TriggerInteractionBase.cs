97ce28daa12ff07a0fb2f4cbcd8c144a10637fe0
KaizDoodle <13iak13@gmail.com>
2024-02-07 09:58:06 -0800

    start input manager. create interface for input

--- test/Assets/Script/Interactions/TriggerInteractionBase.cs
+++ test/Assets/Script/Interactions/TriggerInteractionBase.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour, IInteractable 
+public class NewBehaviourScript : MonoBehaviour, IInteractable
 {
+    public GameObject Player { get; set;}
+    public bool CanInteract { get; set; }
+
+    private void Start() 
+    {
+        Player = GameObject.FindGameObjectsWithTag("Player");
+    }
+    public void Interact() {}
 
 }
--- test/Assets/Script/Interface/IInteractable.cs
+++ test/Assets/Script/Interface/IInteractable.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IInteractable : MonoBehaviour
+public interface IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    GameObject Player {get; set;}
+    bool CanInteract {get; set;}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Interact();
 }
