82a618128b6d4be4ca85ef989bfe46954905bb44
KaizDoodle <13iak13@gmail.com>
2024-03-06 09:52:41 -0800

    wip commit fix anamations miko has sent

--- test/Assets/Script/Player/PlayerController.cs
+++ test/Assets/Script/Player/PlayerController.cs
@@ -7,14 +7,14 @@ using UnityEngine;
         [SerializeField] private float _moveSpeed = 3f;
         private Vector2 _movement;
         private Rigidbody2D _rb;
-        //private Animator _animator;
+        private Animator _animator;
         private const string _horizontal = "Horizontal";
         private const string _vertical = "Vertical";
 
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
-            //_animator = GetComponent<Animator>();
+            _animator = GetComponent<Animator>();
         }
 
 
@@ -23,8 +23,8 @@ using UnityEngine;
             _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
             _rb.velocity = _movement * _moveSpeed;
 
-            //_animator.SetFloat(_horizontal, _movement.x);
-            //_animator.SetFloat(_vertical, _movement.y);
+            _animator.SetFloat(_horizontal, _movement.x);
+            _animator.SetFloat(_vertical, _movement.y);
 
         }
     }
