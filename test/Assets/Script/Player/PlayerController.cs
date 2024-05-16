b32b26005f8ba30cd38f26adfc57547413c474cc
KaizDoodle <13iak13@gmail.com>
2024-05-16 11:39:04 -0700

    a bunch of changes started fixing movmenet script

--- test/Assets/Script/Player/PlayerController.cs
+++ test/Assets/Script/Player/PlayerController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private float _moveSpeed = 3f;
+        
         private Vector2 _movement;
+        
         private Rigidbody2D _rb;
         private Animator _animator;
+
         private const string _horizontal = "Horizontal";
         private const string _vertical = "Vertical";
-
+        private const string _lastHorizontal = "LastHorizontal";
+        private const string _lastVertical = "LastVertical";
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
@@ -25,6 +29,12 @@ using UnityEngine;
 
             _animator.SetFloat(_horizontal, _movement.x);
             _animator.SetFloat(_vertical, _movement.y);
+
+            if (_movement != Vector2.zero){
+                _animator.SetFloat(_lastHorizontal, _movement.x);
+                _animator.SetFloat(_lastVertical, _movement.y);
+                
+            }
 
         }
     }
